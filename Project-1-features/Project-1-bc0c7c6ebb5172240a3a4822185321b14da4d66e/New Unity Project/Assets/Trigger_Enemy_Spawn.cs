92232a9bf01c1ab4340ae11ceb79a5dce59996a2
SuspectPlayer <tomtomcoromandel@gmail.com>
2020-08-07 14:44:42 +1000

    Added AI Spawn Triggers
    Sometimes the AI spawns Dead... WTF?

---
+++ Project-1-features/Project-1-bc0c7c6ebb5172240a3a4822185321b14da4d66e/New Unity Project/Assets/Trigger_Enemy_Spawn.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Trigger_Enemy_Spawn : MonoBehaviour
+{
+    public GameObject enemyAI;
+    public Transform spawnLocation;
+    public Transform spawnLocation2;
+    public Transform spawnLocation3;
+    public Transform spawnLocation4;
+
+
+    private bool done;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" )
+        {
+            SpawnEnemy();
+            DestroySelf();
+        }
+    }
+    private void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+
+    private void SpawnEnemy()
+    {
+        if (!done)
+        {
+            Instantiate(enemyAI, spawnLocation.transform.position, spawnLocation.rotation);
+            done = true;
+            if (spawnLocation2 != null)
+            {
+                {
+                    Instantiate(enemyAI, spawnLocation2.transform.position, spawnLocation2.rotation);
+                    Debug.Log("spawn 2 present");
+                }
+            }
+            if (spawnLocation3 != null)
+            {
+                {
+                    Instantiate(enemyAI, spawnLocation3.transform.position, spawnLocation3.rotation);
+                    Debug.Log("spawn 3 present");
+                }
+            }
+            if (spawnLocation4 != null)
+            {
+                {
+                    Instantiate(enemyAI, spawnLocation4.transform.position, spawnLocation4.rotation);
+                    Debug.Log("spawn 4 present");
+                }
+            }
+        }
+    }
+}
